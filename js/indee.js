25526c5f0e2486019cff88566b50d9e58b4d673a
juals <juanals5678910@gmail.com>
2020-07-15 15:36:23 -0400

    mod producto

---
+++ js/indee.js
@@ -0,0 +1,77 @@
+//const SERVER='https://apiesamweb.esamcca.com/graphql';
+const SERVER = 'https://cors-anywhere.herokuapp.com/http://cybercorp.herokuapp.com/graphql/';
+document.addEventListener('DOMContentLoaded', function () {
+    app = new Vue({
+        el: '#app',
+        data: {
+            idproducto: -1,
+            productoseleccionado:{},
+            categorias:[],
+        },
+        methods: {
+
+            getproductos: function (id) {
+                console.log("edithqqq", id)
+                var sql = `query{
+						producto(id:`+ id + `){id nombre descripcion imagenUrl categoria{
+                            nombre
+                          } }
+						
+					  }`;
+                console.log("ee", sql)
+                axios.post(SERVER, {
+                    query: sql
+                }).then((res) => {
+                    lista = res.data.data.producto;
+                    console.log("hola edith", lista);
+                    this.productoseleccionado = lista;
+                    window.localStorage.setItem('productoseleccionado', JSON.stringify(lista));
+                   
+                    
+
+
+                }).catch((error) => {
+                    console.error(error);
+                })
+
+            },
+
+
+
+
+        },
+        getcategorias: function(){
+            axios.post(SERVER,{
+                query: `query{
+                    categorias{
+                      id
+                      nombre
+                      subcategorias{
+                        nombre
+                      }							 						
+                    }
+                  }`
+            }).then((res)=>{
+                lista = res.data.data.categorias;
+                
+                this.categorias = lista;
+            
+            }).catch((error)=>{
+                console.error(error);
+            })
+
+        },
+        mounted() {
+
+            
+            
+            this.idproducto = localStorage.getItem('idproducto');
+                console.log("erer", this.idproducto);
+                this.getproductos(this.idproducto);
+                this.getcategorias();
+            /*this.getsubcategorias();*/
+            /*$('.gallery a').simpleLightbox({});*/
+        }
+    });
+});
+
